60d81ef54cc0dc55e46da08589863084a267891f
Ghanshyam Tanchak <ghtanchak77@gmail.com>
2021-11-28 23:18:10 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-2xl">lets build airbnb</h1>
+    </div>
+  );
+}
