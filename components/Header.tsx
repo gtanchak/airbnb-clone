07d812d5f9902c8832518d3a9378fd1909d70bf3
Ghanshyam Tanchak <ghtanchak77@gmail.com>
2021-12-01 21:03:04 +0530

    header layout style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
-import { SearchIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+  UserIcon,
+} from "@heroicons/react/solid";
 import { AirbnbLogo } from "../public/assets/images";
 
 const Header = () => {
@@ -22,7 +28,14 @@ const Header = () => {
         />
         <SearchIcon className="hidden md:inline-flex bg-red-400 h-8 p-2 rounded-full text-white cursor-pointer md:mx-2" />
       </div>
-      <div></div>
+      <div className="flex items-center justify-end space-x-4 text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Becom a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 };
