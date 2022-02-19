74d25af4d905c55e3133c9ba8ec098b753e3275b
Ghanshyam Tanchak <ghtanchak77@gmail.com>
2022-02-19 20:15:07 +0530

    lint fix

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 import { BannerImg } from 'assets/images';
 
 const Banner = () => (
-    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
-      <Image src={BannerImg} layout="fill" objectFit="cover" />
-      <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
-        <button type="button" className="button">I'm flexible</button>
-      </div>
+  <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+    <Image src={BannerImg} layout="fill" objectFit="cover" />
+    <div className="absolute top-1/2 w-full text-center">
+      <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+      <button type="button" className="button">
+        I&aposm flexible
+      </button>
     </div>
-  );
+  </div>
+);
 
 export default Banner;
--- src/components/MediumCard.tsx
+++ src/components/MediumCard.tsx
@@ -1,20 +1,18 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
-import { ICardData } from '../interfaces';
+import { ICardData } from 'interfaces';
 
 interface Props {
   data: ICardData;
 }
 
-const MediumCard: NextPage<Props> = function ({ data: { img, title } }) {
-  return (
-    <div className="medium-card">
-      <div className="relative h-80 w-80">
-        <Image src={img} layout="fill" className="rounded-xl" />
-      </div>
-      <h3 className="text-2xl mt-3">{title}</h3>
+const MediumCard: NextPage<Props> = ({ data: { img, title } }) => (
+  <div className="medium-card">
+    <div className="relative h-80 w-80">
+      <Image src={img} layout="fill" className="rounded-xl" />
     </div>
-  );
-};
+    <h3 className="text-2xl mt-3">{title}</h3>
+  </div>
+);
 
 export default MediumCard;
