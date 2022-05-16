58fcc2e64a38c10141a3acebe0e160ddf6cddcfd
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-05-16 21:25:44 +0530

    global style for button

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -11,9 +11,8 @@ const Search: NextPage = () => (
         <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
 
         <div className="hidden lg:inline-flex">
-          <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95 active:bg-gray-100 transition transform duration-100">
-            Cancellation Flexibility
-          </p>
+          <p className="searchButton">Cancellation Flexibility</p>
+          <p className="searchButton">Type of Place</p>
         </div>
       </section>
     </main>
