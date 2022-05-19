5e2d9fc0e8801b0d4a81ea9139b99a5063e70783
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-05-19 21:04:18 +0530

    added formated date for search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,20 +1,32 @@
 import Footer from "components/Footer";
 import Header from "components/Header";
+import { format } from "date-fns";
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 
 const Search: NextPage = () => {
   const router = useRouter();
 
-  console.log(router.query);
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate =
+    startDate && format(new Date(startDate.toString()), "dd MMM yy");
+  const formattedEndDate =
+    endDate && format(new Date(endDate.toString()), "dd MMM yy");
+
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
 
   return (
     <div>
       <Header />
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
-          <p className="text-sm">300+ Stays for 5 number of guest</p>
-          <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Mars</h1>
+          <p className="text-sm">
+            300+ Stays - {range} - for {noOfGuests} guest
+          </p>
+          <h1 className="text-3xl font-semibold mt-2 mb-6">
+            Stays in {location}
+          </h1>
 
           <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
             <p className="searchButton">Cancellation Flexibility</p>
